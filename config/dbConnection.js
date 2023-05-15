c511346624c01c5b590de997fec354c31c1db13f
Sireethon Bunchongpru <werrty65@gmail.com>
2023-05-15 23:41:45 +0700

    Change MongoDB config

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const connectDb = async () => {
     try {
 
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
         console.log("Database Connected", 
         connect.connection.host,
         connect.connection.name
