23640bc75a2b0712cca584b476a5231e9229a082
Sireethon Bunchongpru <werrty65@gmail.com>
2023-05-24 17:08:10 +0700

    Update Food Api

--- model/foodModel.js
+++ model/foodModel.js
@@ -62,3 +62,14 @@ const foodSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('Food', foodSchema);
+
+/*
+    name : Tofu-Burger,
+    image : "unsplash",
+    rating : 5,
+    kcl : 500,
+    meal : "lunch",
+    type: "Drinking",
+    ingredients : [{name: "bacon", quantity: "2 slices"}, {name: "cheese", quantity: "2 slices"}],
+    howto : [{step: "step1"}, {step: "step2"}]
+*/
