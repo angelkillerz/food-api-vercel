8136818365d8a38c67fd0838451ad36ddb3df520
Sireethon Bunchongpru <werrty65@gmail.com>
2023-05-24 16:56:55 +0700

    Update route

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getFoods, insertFood} = require('../Controller/foodController')
+const {getFoods, insertFood, getOneFood} = require('../Controller/foodController')
 
 //Route
 
 router.route('/create').post(insertFood)
 router.route('/get').get(getFoods)
+router.route('/get/:id').get(getOneFood)
                  
 
 module.exports = router;
